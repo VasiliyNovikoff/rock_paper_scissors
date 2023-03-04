85985b9d72af1276fa03829f89f32bd8b286203b
Vasiliy Novikov <novikoffvas@mail.ru>
2023-03-04 19:38:42 +0300

    добавил конфигурацию для бота

--- bot.py
+++ bot.py
@@ -1,56 +1,48 @@
+import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart, Text
-from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Message)
-from config_data.config import load_config
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from config_data.config import Config, load_config
+from handlers import user_handlers, other_handlers
 
 
-# В качестве переменной для фукции указывается путь к .env
-# Или ничего, если в одной папке с текущим файлом
-config = load_config()
-
-bot_token = config.tg_bot.token
-
-bot: Bot = Bot(token=bot_token)
-dp: Dispatcher = Dispatcher()
-
-# Инициализируем объект билдера
-kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
-
-# Создаём список с кнопками
-buttons_1: list[KeyboardButton] = [KeyboardButton(text=f'Кнопка {i}') for i in range(1, 11)]
-
-# Методами билдера добавляем в него кнопки (метод .row())
-# kb_builder.row(*buttons_1, width=4)
-
-# Распаковываем список кнопок методом add
-kb_builder.add(*buttons_1)
-
-# Сообщаем билдеру, сколько хотим видеть кнопок в первом и втором ряду
-kb_builder.adjust(2, 1, repeat=True)
-
-# Методом as_markup() передаем клавиатуру, где она требуется
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
 
 
-# Этот хэндлер будет срабатывать на комманду /start и отправлять в чат клавиатуру
-@dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(text='Вот такая клавиатура получилась',
-                         reply_markup=kb_builder.as_markup(resize_keyboard=True))
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
 
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting Bot!')
 
-# Этот хэндлер будет срабатывать на ответ "Собак" и удалять клавиатуру
-@dp.message(Text(text='Собак'))
-async def process_dog_answer(message: Message):
-    await message.answer(text='Несомнемненно, кошки боятся собак.\n'
-                              'Но видили как они пугаются огурцов!')
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
 
+    # Инициализируем бот и диспетчер
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
 
-# Этот хэндлер будет срабатывать на ответ "Огурцов" и удалять клавиатуру
-@dp.message(Text(text='Огурцов'))
-async def process_cucumber_answer(message: Message):
-    await message.answer(text='Да, иногда кажется, кошки боятся больше огурцов, чем собак!')
+    # Регистрируем роутеры в диспетчер
+    dp.include_router(user_handlers.router)
+    dp.include_routers(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    try:
+        # Запускаем функцию main в асинхронном режиме
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        # Выводим в консоль сообщение об ошибке,
+        # если получены исключения KeyboardInterrupt или SystemExit
+        logger.error('Бот остановлен!')
